1cb311796a30924723e565892bd1fc096940037a
sunilbhave <sunilbhave@atriina.com>
2024-05-10 15:01:33 +0530

    mendix customized docker db

--- atrina_crud1/project/native/styles.js
+++ atrina_crud1/project/native/styles.js
@@ -1,10 +1,11 @@
 import * as style0 from "C:/Users/User/Mendix/Atrina-main/themesource/administration/native/main";
-import * as style1 from "C:/Users/User/Mendix/Atrina-main/themesource/feedbackmodule/native/main";
-import * as style2 from "C:/Users/User/Mendix/Atrina-main/themesource/nanoflowcommons/native/main";
-import * as style3 from "C:/Users/User/Mendix/Atrina-main/themesource/atlas_core/native/main";
-import * as style4 from "C:/Users/User/Mendix/Atrina-main/themesource/home/native/main";
-import * as style5 from "C:/Users/User/Mendix/Atrina-main/theme/native/main";
+import * as style1 from "C:/Users/User/Mendix/Atrina-main/themesource/externaldatabaseconnector/native/main";
+import * as style2 from "C:/Users/User/Mendix/Atrina-main/themesource/feedbackmodule/native/main";
+import * as style3 from "C:/Users/User/Mendix/Atrina-main/themesource/nanoflowcommons/native/main";
+import * as style4 from "C:/Users/User/Mendix/Atrina-main/themesource/atlas_core/native/main";
+import * as style5 from "C:/Users/User/Mendix/Atrina-main/themesource/home/native/main";
+import * as style6 from "C:/Users/User/Mendix/Atrina-main/theme/native/main";
 
 import { flatten } from "mendix/native";
 
-module.exports = flatten([style0, style1, style2, style3, style4, style5]);
+module.exports = flatten([style0, style1, style2, style3, style4, style5, style6]);
